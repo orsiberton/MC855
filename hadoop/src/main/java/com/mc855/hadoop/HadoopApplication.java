28bf1bb103cdb34feb163823e492c99390bb5776
bberton-ciandt <bberton@ciandt.com>
2017-08-13 10:11:52 -0300

    initial spring project

---
+++ hadoop/src/main/java/com/mc855/hadoop/HadoopApplication.java
@@ -0,0 +1,12 @@
+package com.mc855.hadoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadoopApplication.class, args);
+	}
+}
